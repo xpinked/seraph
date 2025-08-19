6cc41eecc78bcf48cedf30be790c6360b074e24b
Udi Shalev <azsd91@gmail.com>
2025-08-19 12:50:08 +0300

    Add environment configuration file and update main.rs to set DATABASE_URL from config

--- migration/src/main.rs
+++ migration/src/main.rs
@@ -1,6 +1,12 @@
 use sea_orm_migration::prelude::*;
+use seraph_backend::config::Config;
 
 #[async_std::main]
 async fn main() {
+    let config = Config::from_env();
+    unsafe {
+        std::env::set_var("DATABASE_URL", &config.db_url);
+    }
+
     cli::run_cli(migration::Migrator).await;
 }
